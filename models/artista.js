6807ce6f1b14a583d7d43a666a2a70a9464f5fb1
Laura <lauracast142@gmail.com>
2024-10-16 19:36:30 -0500

    imagenes en modelo artista

--- models/artista.js
+++ models/artista.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const artistaSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   nacionalidad: { type: String },
+  imagen: { type: String, required: true }, 
 });
 
 const Artista = mongoose.model("Artista", artistaSchema);
